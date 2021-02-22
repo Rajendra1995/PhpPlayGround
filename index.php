458a2d11aec5e7ef5af8dc782db640485aa29b23
RajendraPatel <www.rajendrapatel@gmail.com>
2021-02-22 15:36:39 +0530

    renamed

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $db_name ="tiffinwala_user";
 $mysql_username ="root";
 $mysql_password = "Raj19950123@";
-$mysql_server = "192.168.56.1";
+$mysql_server = "127.0.0.1";
   
 $conn = mysqli_connect($mysql_server,$mysql_username,$mysql_password,$db_name); 
 if ($conn){
