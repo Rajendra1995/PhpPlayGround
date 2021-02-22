27b71a85b9b54ba72ec02af092432ebf9c825f76
RajendraPatel <www.rajendrapatel@gmail.com>
2021-02-22 19:40:33 +0530

    database info changed

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-$db_name ="tiffinwala_user";
-$mysql_username ="root";
-$mysql_password = "Raj19950123@";
-$mysql_server = "127.0.0.1";
+$db_name ="heroku_429b3201452b3e9";
+$mysql_username ="b5a4f0b6c87ada";
+$mysql_password = "4fe9ab00";
+$mysql_server = "us-cdbr-east-03.cleardb.com";
   
 $conn = mysqli_connect($mysql_server,$mysql_username,$mysql_password,$db_name); 
 if ($conn){
